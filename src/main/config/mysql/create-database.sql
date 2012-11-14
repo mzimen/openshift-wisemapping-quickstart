fc77ac12a514867387a8d1009eb072e9f30b587d
Michal Zimen <mzimen@redhat.com>
2012-11-14 14:14:20 +0100

    Changed localhost to 127.0.0.1

--- src/main/config/mysql/create-database.sql
+++ src/main/config/mysql/create-database.sql
@@ -1,3 +1,3 @@
 CREATE DATABASE wisemapping CHARACTER SET='utf8' COLLATE='utf8_unicode_ci';
-CREATE USER 'wisemapping'@'localhost' IDENTIFIED BY 'password';
-GRANT ALL ON wisemapping.* TO 'wisemapping'@'localhost';
+CREATE USER 'wisemapping'@'127.0.0.1' IDENTIFIED BY 'password';
+GRANT ALL ON wisemapping.* TO 'wisemapping'@'127.0.0.1';
