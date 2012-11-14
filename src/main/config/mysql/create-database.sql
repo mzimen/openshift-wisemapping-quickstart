ab30e6256f006ebb06552f1244fabe954ef0ec12
Michal Zimen <mzimen@redhat.com>
2012-11-14 14:23:58 +0100

    Enabled MySQL instead of HSQL

--- src/main/config/mysql/create-database.sql
+++ src/main/config/mysql/create-database.sql
@@ -1,3 +1,5 @@
 CREATE DATABASE wisemapping CHARACTER SET='utf8' COLLATE='utf8_unicode_ci';
 CREATE USER 'wisemapping'@'127.0.0.1' IDENTIFIED BY 'password';
+CREATE USER 'admin'@'127.0.0.1' IDENTIFIED BY 'RtpGzRgv7Ndv';
 GRANT ALL ON wisemapping.* TO 'wisemapping'@'127.0.0.1';
+GRANT ALL ON wisemapping.* TO 'admin'@'127.0.0.1';
